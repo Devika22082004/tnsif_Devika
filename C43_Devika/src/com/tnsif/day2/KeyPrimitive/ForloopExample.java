faa57d18cc8b81ca8ad662cd2c0dec956863270f
DEVIKA <DEVIKA@DESKTOP-MCJUEJN>
2024-08-21 11:02:05 -0700

    commit5

--- C43_Devika/src/com/tnsif/day2/KeyPrimitive/ForloopExample.java
+++ C43_Devika/src/com/tnsif/day2/KeyPrimitive/ForloopExample.java
@@ -1,5 +1,10 @@
 package com.tnsif.day2.KeyPrimitive;
 
-public class ForloopExample {
-
-}
+public class ForloopExample {public static void main(String[] args) {
+	 
+	for(int i = 1; i<=1000 ; i++)  
+	{	
+		System.out.print("Value of i:");
+		System.out.println(i);
+	}
+}}
